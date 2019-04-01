3f9f8c55985faaa76a252478489ab2726f0c3cc8
Joe Tustin <j.tustin@gmail.com>
2019-04-01 16:27:16 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,7 @@
 # require modules here
 
 require 'yaml'
-emoticons = YAML.load_file('emoticons.yml')
+emoticons = YAML.load_file('lib/emoticons.yml')
 #YAML.load_file('emoticons.yml')
 
  puts emoticons
