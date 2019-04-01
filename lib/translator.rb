924aa34e9d6f3d6c453879388d3262572c52f273
Joe Tustin <j.tustin@gmail.com>
2019-04-01 18:41:46 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -9,17 +9,9 @@ def load_library(file_destination)
   emoticon_raw = YAML.load_file(file_destination)
 
 
-#  emoticon_format = Hash.new
   emoticon_format = {:get_meaning => {}, :get_emoticon => {}}
-binding.pry
 
-  #emoticon_hash.each do |e_meaning, e_j_emoticon|
 
-  #  get_meaning = get_meaning.merge(e_meaning)
-  #  get_emoticon.merge(e_j_emoticon)
-#  end
-
-#  return emoticon_hash
 end
 
 def get_japanese_emoticon
