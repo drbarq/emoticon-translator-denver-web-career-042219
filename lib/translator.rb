e80115dc568f7fd92dbc26aa7101a6f09d3406cb
Joe Tustin <j.tustin@gmail.com>
2019-04-01 18:38:47 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -12,7 +12,7 @@ def load_library(file_destination)
 
 
   #emoticon_hash.each do |e_meaning, e_j_emoticon|
-  #  binding.pry
+    binding.pry
   #  get_meaning = get_meaning.merge(e_meaning)
   #  get_emoticon.merge(e_j_emoticon)
 #  end
